bf250b1aae359dd7fd2a9ae7fd1f85455547c9de
Kent Ogden <ogdenk@upstate.edu>
2016-11-08 11:00:36 -0500

    Updated to change checking of command line for resampling option

--- RescaleResample.cxx
+++ RescaleResample.cxx
@@ -6,7 +6,6 @@
 //  The label map will also be resampled to match the origin, spacing, direction, and matrix of the MRI scan.  This is required by some ITK filters
 //  used to extract radiomics features from the MRI scan
 
-
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkNearestNeighborInterpolateImageFunction.h"
@@ -15,6 +14,7 @@
 #include "itkBinaryMedianImageFilter.h"
 #include "itkShiftScaleImageFilter.h"
 #include <vector>
+#include <math.h>
 
 // These are the Freesurfer label values for the structures of interest
 #define LeftHippocampus 17
@@ -22,24 +22,21 @@
 #define LeftThalamus 10
 #define RightThalamus 49
 
-
-
 int main(int argc, char * argv[])
 {
-  
   const unsigned int Dimension = 3;
-  size_t foundchar;  // for filename parsing
+  size_t foundchar;  // for fileNameNoExt parsing
   unsigned short int labelValue; // holds the current label value during the loop
-  float sum=0, mean=0, factor=1,temp;  // used for rescaling the MRI 
+  float sum=0, mean=0, factor=1;  // used for rescaling the MRI 
+  bool interpolate = false;
  
-
   // both image data and labels can be opened as unsigned short
   typedef  unsigned short  int								   PixelType;
   typedef itk::Image< PixelType, Dimension >                   ImageType;
   int i = 0;  // just a counter
   
   // the file names for the brain scan, label image, and output file
-  std::string imageData, filename, labelData, Path, outputLabels, outputImage;
+  std::string inputImageFile, fileNameNoExt, inputLabelFile, Path, outputLabelFile, outputImageFile;
   
   ImageType::SizeType medianRadius; // used for binary median image filter
   medianRadius[0] = 1;
@@ -53,12 +50,11 @@ int main(int argc, char * argv[])
   labelValues.push_back(LeftThalamus);
   labelValues.push_back(RightThalamus);
 
-
    // Get the input file specified on the command line
   
   if (argc > 1)
   {
-	 	imageData = argv[1];
+	 	inputImageFile = argv[1];
   }
   else
   {
@@ -66,35 +62,37 @@ int main(int argc, char * argv[])
 	std::cout << std::endl;
   	std::cout <<  "Input an image file to resample the corresponding label file to match." << std::endl;
 	std::cout << "The label file should be in the same directory and have the name:" << std::endl;
-	std::cout << "<filename>_label.nrrd where <filename> is the input file." << std::endl;
-	std::cout << "The output filename will be <filename>_label_resamp.nrrd" << std::endl;
+	std::cout << "<fileName>_label.nrrd where <fileName> is the input file." << std::endl;
+	std::cout << "The output label map file will be <fileName>_label_resamp.nrrd" << std::endl;
+	std::cout << "To cause the label map to be resampled, put 'resample' after the input file name." << std::endl;
+	std::cout << "If there is nothing after the file name, the input file will still be rescaled." << std::endl;
 	std::cout << std::endl;
 	std::cout << std::endl;
 	exit(1);
   }
 
-  
+  if (argc > 2)  interpolate = true; // anything on the command line after the file name will cause labelmap interpolation
 
-  foundchar = imageData.find_last_of("/\\");
-  Path = imageData.substr(0, foundchar+1);
+  foundchar = inputImageFile.find_last_of("/\\");
+  Path = inputImageFile.substr(0, foundchar+1);
 
-  filename = imageData.substr(foundchar + 1);
-  foundchar = filename.find_last_of(".");
-  labelData = Path + filename.substr(0, foundchar) +"_label.nrrd";
+  fileNameNoExt = inputImageFile.substr(foundchar + 1);
+  foundchar = fileNameNoExt.find_last_of(".");
+  inputLabelFile = Path + fileNameNoExt.substr(0, foundchar) +"_label.nrrd";
 
-  outputLabels = Path + filename.substr(0, foundchar) + "_rescaled_label.nrrd";
-  outputImage = Path + filename.substr(0, foundchar) + "_rescaled.nrrd";
+  outputLabelFile = Path + fileNameNoExt.substr(0, foundchar) + "_rescaled_label.nrrd";
+  outputImageFile = Path + fileNameNoExt.substr(0, foundchar) + "_rescaled.nrrd";
 
   // open the image data
   typedef itk::ImageFileReader< ImageType >  ReaderType;
   ReaderType::Pointer imageReader = ReaderType::New();
-  imageReader->SetFileName(imageData );
+  imageReader->SetFileName(inputImageFile );
   imageReader->Update();
 
   // open the labels
   typedef itk::ImageFileReader< ImageType >  LabelReaderType;
   LabelReaderType::Pointer labelReader = LabelReaderType::New();
-  labelReader->SetFileName(labelData);
+  labelReader->SetFileName(inputLabelFile);
   labelReader->Update();
 
   // check image geometries
@@ -107,6 +105,23 @@ int main(int argc, char * argv[])
   ImageType::PointType origin = imageReader->GetOutput()->GetOrigin();
   ImageType::PointType labelorigin = labelReader->GetOutput()->GetOrigin();
 
+  if (!interpolate)
+  {
+	  if (abs(int(size[0] - labelsize[0])) > 0 | abs(int(size[1] - labelsize[1])) > 0 | abs(int(size[2] - labelsize[2])) > 0 |
+		  abs(spacing[0] - labelspacing[0]) > 0.01 | abs(spacing[1] - labelspacing[1]) > 0.01 | abs(spacing[2] - labelspacing[2]) > 0.01 |
+		  abs(origin[0] - labelorigin[0]) > 0.01 | abs(origin[1] - labelorigin[1]) > 0.01 | abs(origin[2] - labelorigin[2]) > 0.01)
+	  {
+		  std::cout << "There is a problem with the data." << std::endl;
+		  std::cout << "Interpolation of the label map data is turned off but the label map" << std::endl;
+		  std::cout << "don't have the same geometry.  Make sure you have interpolation turned on" << std::endl;
+		  std::cout << "if the label map and image data have different size, spacing, or origin." << std::endl;
+		  exit(1);
+	  }
+	  labelReader->GetOutput()->SetSpacing(imageReader->GetOutput()->GetSpacing());
+	  labelReader->GetOutput()->SetOrigin(imageReader->GetOutput()->GetOrigin());
+	
+  }
+  
   // Need an interpolator, should use nearest neighbor for label maps
   typedef itk::NearestNeighborInterpolateImageFunction<ImageType, double >  InterpolatorType;
   InterpolatorType::Pointer interpolator = InterpolatorType::New();
@@ -120,17 +135,23 @@ int main(int argc, char * argv[])
   resampler->SetOutputSpacing(spacing);
   resampler->SetOutputOrigin(origin);
   resampler->SetOutputDirection(imageReader->GetOutput()->GetDirection());
-  resampler->Update();
+  
 
   typedef itk::ImageFileWriter<ImageType> WriterType;
   WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName(outputLabels);
-  writer->SetInput(resampler->GetOutput());
-  writer->Update();
-  writer->SetFileName(labelData);
-  writer->Update(); // Save two copies with different names to support the RFE program which uses the input name
-					// to derive the label file name.  Original label file gets overwritten with resampled image
+  writer->SetFileName(outputLabelFile);
   
+  if(interpolate)
+	  writer->SetInput(resampler->GetOutput());
+  else
+	  writer->SetInput(labelReader->GetOutput()); // if no interpolate then just copy the input file
+  writer->Update();
+
+  if (interpolate) // only need to overwrite the input label file if interpolation occurred
+  {
+	  writer->SetFileName(inputLabelFile);
+	  writer->Update(); // Save two copies with different names to support the RFE program which uses the input name
+  }					// to derive the label file name.  Original label file gets overwritten with resampled image
 
   // Now we use the resampled label image to get the stats for the four structures
 
@@ -148,7 +169,6 @@ int main(int argc, char * argv[])
   BinaryToStatisticsFilter->SetComputeHistogram(TRUE);
   BinaryToStatisticsFilter->SetCoordinateTolerance(0.01);
   BinaryImageToStatisticsLabelMapFilterType::OutputImageType::LabelObjectType* StatlabelObject;
-
 
   std::vector<unsigned short int>::iterator labelValuesIterator = labelValues.begin();
 
@@ -178,7 +198,7 @@ int main(int argc, char * argv[])
   scaler->SetShift(0);
   scaler->Update();
 
-  writer->SetFileName(outputImage);
+  writer->SetFileName(outputImageFile);
   writer->SetInput(scaler->GetOutput());
   writer->Update();
  
